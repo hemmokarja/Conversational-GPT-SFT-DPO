7636cf68e8dd7a01d6c84d8c4891b4b1c29e1c8f
Hemmo Karja <hemmo.karja@sok.fi>
2025-08-06 19:21:22 +0300

    Add util functions for adding pad and im tokens

--- src/text_util.py
+++ src/text_util.py
@@ -2,6 +2,28 @@ import re
 
 import torch
 
+PAD_TOKEN = "<|pad|>"
+IM_START_TOKEN = "<|im_start|>"
+IM_END_TOKEN = "<|im_end|>"
+
+
+def add_pad_token_to_tokenizer(tokenizer, pad_token=PAD_TOKEN):
+    tokenizer.add_special_tokens({"pad_token": pad_token})
+
+
+def add_chatml_tokens_to_tokenizer(
+    tokenizer, im_start_token=IM_START_TOKEN, im_end_token=IM_END_TOKEN
+):
+    tokenizer.add_special_tokens(
+        {"additional_special_tokens": [im_start_token, im_end_token]}
+    )
+
+    tokenizer.im_start_token = im_start_token
+    tokenizer.im_end_token = im_end_token
+
+    tokenizer.im_start_token_id = tokenizer.convert_tokens_to_ids(im_start_token)
+    tokenizer.im_end_token_id = tokenizer.convert_tokens_to_ids(im_end_token)
+
 
 def decode_by_token(tokenizer, ids, **tokenizer_kwargs):
     # avoid spaces around ChatML im tokens, can't get rid of them when applying calling
